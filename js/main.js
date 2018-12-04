ce862193d2dcc44024be4b80243ca0518947df9c
Lucas Farias <lfg.farias@gmail.com>
2018-12-04 15:59:10 -0300

    Rolagem para o formulário

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-// Rolamento deslizante
+// Rolamento deslizante por link
 $('a[href*="#"]').on('click', function (e) {
 	e.preventDefault();
 
 	$('html, body').animate({
 		scrollTop: $($(this).attr('href')).offset().top
 	}, 500, 'linear');
+});
+
+//Rolagem deslizante por botão
+$(".btn-confirmar").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#event-form").offset().top
+    }, 1000);
 });
 
 // Girar fotos em ação do hover
